71c7e7f686da525d209b87d7db6e3aa3a636d2b1
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-05-16 21:23:26 -0500

    feat: Update ChatScreen to display messages from ChatProvider

--- lib/src/screens/chat/chat_screen.dart
+++ lib/src/screens/chat/chat_screen.dart
@@ -3,6 +3,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/src/providers/chat_provider.dart';
 import 'package:yes_no_app/src/providers/user_provider.dart';
 import 'package:yes_no_app/src/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/src/widgets/chat/my_message_bubble.dart';
@@ -43,16 +45,21 @@ class _chatView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: Column(
         children: [
           Expanded(
             child: ListView.builder(
+              itemCount: chatProvider.messagesList.length,
               itemBuilder: (context, index) {
-                return (index % 2 == 0)
+                final message = chatProvider.messagesList[index];
+
+                return (message.fromWho == FromWho.her)
                     ? const HerMessageBubble()
-                    : const MyMessageBubble();
+                    : MyMessageBubble(message: message);
               },
             ),
           ),
