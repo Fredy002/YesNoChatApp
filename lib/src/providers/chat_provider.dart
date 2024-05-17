ec6206461a6eb635f21542dd673952aea29bb4c5
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-05-16 21:23:39 -0500

    refactor: Rename message variable to messagesList in ChatProvider

--- lib/src/providers/chat_provider.dart
+++ lib/src/providers/chat_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  List<Message> messagesList = [
     Message(
       text: 'Welcome!!',
       fromWho: FromWho.me,
