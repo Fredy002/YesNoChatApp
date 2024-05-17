18491149b796c5ac2eb066f57e19737cb36bcffb
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-05-16 19:49:23 -0500

    feat: Add MessageFieldBox widget for user input in ChatScreen

---
+++ lib/src/widgets/shared/message_field_box.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textController = TextEditingController();
+
+    final outlineInputBorder = UnderlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.transparent,
+      ),
+      borderRadius: BorderRadius.circular(20),
+    );
+
+    final inputDecoration = InputDecoration(
+      hintText: 'End your message with a "?"',
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        onPressed: () {},
+        icon: IconButton(
+          icon: const Icon(Icons.send_outlined),
+          onPressed: () {
+            final textValue = textController.value.text;
+            print('button $textValue');
+            textController.clear();
+          },
+        ),
+      ),
+    );
+
+    return TextFormField(
+      controller: textController,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print('Submid value $value');
+      },
+    );
+  }
+}
